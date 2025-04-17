49b3ff9c64b9b123e991c08cd8d19ee32503bcf6
bjornxzz <andrealmeidaaz105@gmail.com>
2025-04-17 19:24:41 -0300

    dia 2

--- mod2/tuple/tuple.js
+++ mod2/tuple/tuple.js
@@ -1,2 +1,9 @@
 "use strict";
-let tuple;
+let pessoa;
+pessoa = ['andre', 'pucminas', 20];
+// pessoa = [20, 'andre', 'pucminas'] /* error sintax 
+console.log(pessoa);
+// demo 2 
+let pessoa1;
+pessoa1 = ['andre', 'pucminas', 20];
+console.log(pessoa1[2]);
--- mod2/tuple/tuple.ts
+++ mod2/tuple/tuple.ts
@@ -1,1 +1,15 @@
-let tuple
+let pessoa: [string, string, number];
+
+pessoa = ['andre', 'pucminas', 20];
+
+// pessoa = [20, 'andre', 'pucminas'] /* error sintax 
+
+console.log(pessoa)
+
+// demo 2 
+
+let pessoa1: [string, string, number];
+
+pessoa1 = ['andre', 'pucminas', 20];
+
+console.log(pessoa1[2])
